5ca2eba37d6bb98d23f138423078ab2b21999168
gazykdeath1104 <148126577+gazykdeath1104@users.noreply.github.com>
2024-02-01 16:20:22 +0200

    add homework 2

---
+++ HomeWork 2/main.js
@@ -0,0 +1,15 @@
+let firstName= prompt('what is your firstName?');
+let secondName= prompt('what is your secondName ?');
+let ages = prompt('How old are you?');
+alert('Your secondName is ' + secondName + '.  Your age ' + ages + '. Your firstName is ' + firstName + '. ');
+
+
+
+
+let digit0 = '4 ';
+let digit1 = '8 ';
+let digit2 = '9 ';
+let digit3 = '1 ';
+let digit4 = '3 ';
+
+alert(digit0 + digit1 + digit2 + digit3 + digit4)
